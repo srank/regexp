70649f849026d219639aaa9691aca76f34b24440
Stephen Rank <Stephen.Rank@gmail.com>
2012-04-02 20:50:31 +0100

    Added ^ handling

--- Grep.hs
+++ Grep.hs
@@ -1,24 +1,27 @@
 module Grep (match) where
 
--- Match the (restricted) regexp r:rs against the string x:xs
--- 
 
-getMatchHere :: String -> String -> Maybe String
-getMatchHere (r:rs) (x:xs)
+matchHere :: String -> String -> Maybe String
+matchHere (r:rs) (x:xs)
  | r `elem` [x, '.'] = appendMatch x result
  | otherwise = Nothing
-   where result = getMatchHere rs xs
+   where result = matchHere rs xs
          appendMatch :: Char -> Maybe String -> Maybe String
          appendMatch x Nothing = Nothing
          appendMatch x (Just xs) = Just (x:xs)
-getMatchHere [] _ = Just []
-getMatchHere _ [] = Nothing
+matchHere [] _ = Just []
+matchHere _ [] = Nothing
+
+
+-- Match the (restricted) regexp r:rs against the string x:xs
+-- Magic characters so far: . ^
 
 match :: String -> String -> Maybe String
-match rs (x:xs)
+match regexp@(r:rs) text@(x:xs)
+ | r == '^' = matchHere rs text
  | isMatch here = here
- | otherwise = match rs xs                                  
-   where here = getMatchHere rs (x:xs)   
+ | otherwise = match regexp xs                                  
+   where here = matchHere regexp (x:xs)   
          isMatch (Just _) = True
          isMatch _ = False
 match rs [] = Nothing                          
--- Tests.hs
+++ Tests.hs
@@ -3,6 +3,9 @@ import Grep (match)
 
 testMatch = match "xy" "123xy456" == Just "xy"
                 && match "x.y" "xzy" == Just "xzy"
+                && match "^1234" "123456" == Just "1234"
+                && match "12^abc" "abc" == Nothing
+                && match "abc^" "abc" == Nothing
                 
 main :: IO ()
 main = print testMatch
