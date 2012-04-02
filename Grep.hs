fac6fd1d6fd0820997cc97862253fbc5752c9c9c
Stephen Rank <Stephen.Rank@gmail.com>
2012-04-02 20:28:28 +0100

    Refactoring

--- Grep.hs
+++ Grep.hs
@@ -1,21 +1,24 @@
 module Grep (match) where
 
-getMatch :: String -> String -> Maybe String
-getMatch (r:rs) (x:xs)
- | r `elem` [x, '.'] = f x result
+-- Match the (restricted) regexp r:rs against the string x:xs
+-- 
+
+getMatchHere :: String -> String -> Maybe String
+getMatchHere (r:rs) (x:xs)
+ | r `elem` [x, '.'] = appendMatch x result
  | otherwise = Nothing
-   where result = getMatch rs xs
-         f :: Char -> Maybe String -> Maybe String
-         f x Nothing = Nothing
-         f x (Just xs) = Just (x:xs)
-getMatch [] _ = Just []
-getMatch _ [] = Nothing
+   where result = getMatchHere rs xs
+         appendMatch :: Char -> Maybe String -> Maybe String
+         appendMatch x Nothing = Nothing
+         appendMatch x (Just xs) = Just (x:xs)
+getMatchHere [] _ = Just []
+getMatchHere _ [] = Nothing
 
 match :: String -> String -> Maybe String
 match rs (x:xs)
  | isMatch here = here
  | otherwise = match rs xs                                  
-   where here = getMatch rs (x:xs)   
+   where here = getMatchHere rs (x:xs)   
          isMatch (Just _) = True
          isMatch _ = False
 match rs [] = Nothing                          
--- Tests.hs
+++ Tests.hs
@@ -1,5 +1,8 @@
-module Tests (testMatch) where
-import Grep
+module Tests (main) where
+import Grep (match)
 
 testMatch = match "xy" "123xy456" == Just "xy"
                 && match "x.y" "xzy" == Just "xzy"
+                
+main :: IO ()
+main = print testMatch
