54342f9da6905e88c24d588cd6a47f58b46203ec
Stephen Rank <Stephen.Rank@gmail.com>
2012-04-02 22:53:44 +0100

    First attempt at * handling

--- Grep.hs
+++ Grep.hs
@@ -6,9 +6,7 @@ matchHere (r:rs) (x:xs)
  | r `elem` [x, '.'] = appendMatch x result
  | otherwise = Nothing
    where result = matchHere rs xs
-         appendMatch :: Char -> Maybe String -> Maybe String
-         appendMatch x Nothing = Nothing
-         appendMatch x (Just xs) = Just (x:xs)
+
 matchHere [] _ = Just []
 matchHere _ [] = Nothing
 
@@ -20,6 +18,7 @@ matchHere _ [] = Nothing
 
 match :: String -> String -> Maybe String
 match regexp@(r:rs) text@(x:xs)
+ | rs /= [] && head rs == '*' = matchStar r rs text
  | r == '^' = matchHere rs text
  | isMatch here = here
  | otherwise = match regexp xs                                  
@@ -27,3 +26,18 @@ match regexp@(r:rs) text@(x:xs)
          isMatch (Just _) = True
          isMatch _ = False
 match rs [] = Nothing                          
+
+matchStar :: Char -> String -> String -> Maybe String
+matchStar r rs (x:xs)
+ | r == x = appendMatch r $ matchStar' r rs xs
+ | otherwise = Nothing
+               where
+                 matchStar' :: Char -> String -> String -> Maybe String
+                 matchStar' s rs (x:xs)
+                   | s == x = appendMatch x $ matchStar' s rs xs
+                   | otherwise = matchHere rs xs
+                 matchStar' s _ [] = Just ""
+                 
+appendMatch :: Char -> Maybe String -> Maybe String
+appendMatch x Nothing = Nothing
+appendMatch x (Just xs) = Just (x:xs)                 
--- Tests.hs
+++ Tests.hs
@@ -6,6 +6,7 @@ testMatch = match "xy" "123xy456" == Just "xy"
                 && match "^1234" "123456" == Just "1234"
                 && match "12^abc" "abc" == Nothing
                 && match "abc^" "abc" == Nothing
+                && match "a*" "aaa" == Just "aaa"
                 
 main :: IO ()
 main = print testMatch
