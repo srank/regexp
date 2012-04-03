a71e1e5e25b7fba1cbdfeedfc035977f9d4296f4
Stephen Rank <Stephen.Rank@gmail.com>
2012-04-03 19:50:09 +0100

    Better test results

--- Tests.hs
+++ Tests.hs
@@ -7,13 +7,22 @@ tests = [("xy", "123xy456", Just "xy"),
          ("12^abc", "abc", Nothing),
          ("abc^", "abc", Nothing),
          ("a*", "aaa", Just "aaa"),
-         ("c*", "abccd", Just "cc"]
+         ("c*", "abccd", Just "cc")]
                 
 runTests :: [(String, String, Maybe String)] -> [(String, String, Maybe String)]
 runTests [] = []
 runTests ((regexp, string, expected):tests)
-  | match regexp string == expected = runTests tests
-  | otherwise = (regexp, string, expected):runTests tests
+  | actual == expected = runTests tests
+  | otherwise = (regexp, string, foldl concatMaybeString Nothing
+                                 [Just "expected: '", 
+                                  expected, Just "', actual: '", actual, Just "'"]):runTests tests
+                where actual = match regexp string
+                      
+concatMaybeString :: Maybe String -> Maybe String -> Maybe String
+concatMaybeString (Just s1) (Just s2) = Just (s1 ++ s2)
+concatMaybeString (Just s1) Nothing = Just s1
+concatMaybeString Nothing (Just s2) = Just s2
+concatMaybeString Nothing Nothing = Nothing
                                             
 main :: IO ()
 main = print $ runTests tests
