9ae5eb04195431b6e00557cc44e21dec05170c53
Stephen Rank <Stephen.Rank@gmail.com>
2012-04-04 22:54:54 +0100

    More type-based stuff

--- Regexp.hs
+++ Regexp.hs
@@ -14,3 +14,16 @@ matchRegexp (Literal r) text@(c:cs)
   | length r > length text = Nothing
   | take (length r) text == r  = Just r
   | otherwise = matchRegexp (Literal r) cs
+
+matchRegexp (Or a b) text
+  | matchRegexp a text /= Nothing = matchRegexp a text
+  | matchRegexp b text /= Nothing = matchRegexp b text
+  | otherwise = Nothing
+
+matchRegexp (OneOrMore x) text = Nothing
+
+matchRegexp (ZeroOrMore x) text = Nothing
+matchRegexp (AtStart x) text = Nothing
+matchRegexp (AtEnd x) text = Nothing
+matchRegexp (OneOf (r:rs)) text = Nothing
+matchRegexp (Optional r) text = Nothing
