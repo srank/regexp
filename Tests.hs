9697a2da2e90157652794db28cfdd5450502d4a3
Stephen Rank <Stephen.Rank@gmail.com>
2012-04-19 23:37:26 +0100

    Moved Tokenise tests to their own module

--- Tests.hs
+++ Tests.hs
@@ -2,23 +2,9 @@ module Tests (main) where
 import Regexp
 import ParseRegexp
 import MatchRegexp
+import TokeniseTests
 import Tokenise
 
-tokeniseTests :: [(String, [Token])]
-tokeniseTests = [("xx", [Text "xx"]),
-                 ("(", [OpenBracket]),
-                 (")", [CloseBracket]),
-                 ("^abc|de+f*$", [Start, Text "abc", Either, Text "de",
-                                  Plus, Text "f", Star, End]),
-                 ("\\$12\\.34", [Text "$12.34"])
-                ]
-
-runTokeniseTests :: [(String, [Token])] -> [(String, [Token], [Token])]
-runTokeniseTests [] = []
-runTokeniseTests ((text, expected):xs)
-  | actual == expected = runTokeniseTests xs
-  | otherwise = (text, actual, expected):runTokeniseTests xs
-    where actual = tokenise text
           
 parseTests :: [([Token], Maybe Regexp)]
 parseTests =
@@ -94,5 +80,5 @@ main :: IO ()
 main = do
         print $ runTests matchHere matchHereTests
         print $ runTests match matchTests
-        print $ runTokeniseTests tokeniseTests
+        print tokeniserTests 
         print $ runParseTests parseTests
---
+++ TokeniseTests.hs
@@ -0,0 +1,21 @@
+module TokeniseTests where
+import Tokenise
+
+
+tokeniserTests = runTokeniseTests tokeniseTests
+
+tokeniseTests :: [(String, [Token])]
+tokeniseTests = [("xx", [Text "xx"]),
+                 ("(", [OpenBracket]),
+                 (")", [CloseBracket]),
+                 ("^abc|de+f*$", [Start, Text "abc", Either, Text "de",
+                                  Plus, Text "f", Star, End]),
+                 ("\\$12\\.34", [Text "$12.34"])
+                ]
+
+runTokeniseTests :: [(String, [Token])] -> [(String, [Token], [Token])]
+runTokeniseTests [] = []
+runTokeniseTests ((text, expected):xs)
+  | actual == expected = runTokeniseTests xs
+  | otherwise = (text, actual, expected):runTokeniseTests xs
+    where actual = tokenise text
